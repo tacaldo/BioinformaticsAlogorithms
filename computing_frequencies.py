f1ac8706d2bc9cfceea55968e1bc633594defa00
tacaldo <tonyacaldo@charter.net>
2016-07-23 15:52:40 -0400

    Untested block of work.

--- computing_frequencies.py
+++ computing_frequencies.py
@@ -1,10 +1,14 @@
 import pattern_to_number
 
 
-def computing_frequencies(text, k): # 
+def computing_frequencies(text, k):  #
     frequency_array = []
-
+    pattern = ''
     for i in range(0, 4 ** k):
         frequency_array.append(0)
+    for i in range(0, len(text) - k):
+        pattern = text[i:k + 1]
+        j = pattern_to_number.pattern_to_number(pattern)
+        frequency_array[j] = int(frequency_array[j]) + 1
 
-    return 0
+    return frequency_array
