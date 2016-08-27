18e809f35e12680cfdbbf38059640b042f51da30
tacaldo <tonyacaldo@charter.net>
2016-08-27 15:45:51 -0400

    Scale Up Behaviors

---
+++ better_clump_finding.py
@@ -0,0 +1,65 @@
+import os
+import computing_frequencies
+import number_to_pattern
+import pattern_to_number
+import tkinter as tk
+from tkinter import filedialog
+
+
+def better_clump_finding(genome_file_path, k, L, t):
+    frequent_patterns = []
+    clump = []
+
+    genome_len = os.path.getsize(genome_file_path)
+    f = open(genome_file_path, 'r')
+
+    frequency_array = []
+    pattern = ''
+    for i in range(0, (4 ** k)):
+        clump.append(0)
+        print('appending 0, at i:' + str(i))
+
+    text = f.read(L)
+    frequency_array = computing_frequencies.computing_frequencies(text, k)
+    for index in range(0, (4 ** k)):
+        if frequency_array[index] >= t:
+            clump[index] = 1
+    f.seek(0, 0)
+    for i in range(0, genome_len - L + 1):  # testing here...
+        first_pattern = f.read(k)
+        index = pattern_to_number.pattern_to_number(first_pattern)
+        frequency_array[index] = frequency_array[index] - 1
+        f.seek(i + L - k)
+        last_pattern = f.read(k)
+        index = pattern_to_number.pattern_to_number(last_pattern)
+        frequency_array[index] = frequency_array[index] + 1
+        if frequency_array[index] >= t:
+            clump[index] = 1
+        f.seek(i + 1)  # position for next read
+
+    for i in range(0, (4 ** k)):
+        if (clump[i] == 1):
+            pattern = number_to_pattern.number_to_pattern(i, k)
+            frequent_patterns.append(pattern)
+    f.close()
+    return frequent_patterns
+
+
+genomeIn = 'CCACGCGGTGTACGCTGCAAAAAGCCTTGCTGAATCAAATAAGGTTCCAGCACATCCTCAATGGTTTCACGTTCTTCGCCAATGGCTGCCGCCAGGTTATCCAGACCTACAGGTCCACCAAAGAACTTATCGATTACCGCCAGCAACAATTTGCGGTCCATATAATCGAAACCTTCAGCATCGACATTCAACATATCCAGCG'
+kIn = 1
+L_In = 5
+tIn = 2
+
+# result = clump_finding(genomeIn, kIn, L_In, tIn)
+
+# print(result)
+
+
+
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
+# f = open(file_path, 'r')
+
+result = better_clump_finding(file_path, kIn, L_In, tIn)
+print(result)
--- clump_finding_scale_up.py
+++ clump_finding_scale_up.py
@@ -1,23 +1,29 @@
+import os
 import computing_frequencies
 import number_to_pattern
 import tkinter as tk
 from tkinter import filedialog
 
 
-def clump_finding(genome, k, L, t):
-    """integers L and t,a k-mer Pattern forms an (L,t)-clump inside
-        a (longer) string Genome if there is an interval of Genome of length L in which
-        this k-mer appears at least t times, k-mer must fit within the interval"""
+def clump_finding_scale_up(genome_file_path, k, L, t):
+
     frequent_patterns = []
     clump = []
-    genome_len = len(genome)
+
+    genome_len = os.path.getsize(genome_file_path)
+    f = open(genome_file_path, 'r')
+
     frequency_array = []
     pattern = ''
     for i in range(0, (4 ** k)):
         clump.append(0)
-    for i in range(0, genome_len - L + 1): # testing here...
-        text = genome[i:i + L]
+        print('appending 0, at i:' + str(i))
+
+    for i in range(0, genome_len - L + 1):  # testing here...
+        # text = genome[i:i + L]
+        text = f.read(L)
         frequency_array = computing_frequencies.computing_frequencies(text, k)
+        f.seek(i + 1);
         for index in range(0, (4 ** k)):
             if frequency_array[index] >= t:
                 clump[index] = 1
@@ -26,7 +32,9 @@ def clump_finding(genome, k, L, t):
         if (clump[i] == 1):
             pattern = number_to_pattern.number_to_pattern(i, k)
             frequent_patterns.append(pattern)
+    f.close()
     return frequent_patterns
+
 
 genomeIn = 'CCACGCGGTGTACGCTGCAAAAAGCCTTGCTGAATCAAATAAGGTTCCAGCACATCCTCAATGGTTTCACGTTCTTCGCCAATGGCTGCCGCCAGGTTATCCAGACCTACAGGTCCACCAAAGAACTTATCGATTACCGCCAGCAACAATTTGCGGTCCATATAATCGAAACCTTCAGCATCGACATTCAACATATCCAGCG'
 kIn = 3
@@ -38,8 +46,12 @@ tIn = 3
 # print(result)
 
 
+
 root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename()
-print(file_path)
+# f = open(file_path, 'r')
+
+result = clump_finding_scale_up(file_path, kIn, L_In, tIn)
+print(result)
 
